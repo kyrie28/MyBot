7a9611eb19ce604acbad1749f4099202d5dc0901
kyrie28 <harryasd@kaist.ac.kr>
2022-04-17 20:02:40 +0900

    음악 재생 시 키워드 검색 기능 추가

--- bot.py
+++ bot.py
@@ -29,27 +29,33 @@ async def hello(ctx):
     await ctx.send(f"안녕하세요, {ctx.message.author.mention}!")
 
 
+@bot.command()
+async def echo(ctx, *user_input):
+    output = "".join(str(s) + " " for s in user_input)[:-1]
+    await ctx.send(f"{ctx.message.author.mention} {output}")
+
+
 @bot.command(aliases=["메", "메이플", "메이플스토리"])
-async def maple(ctx, charName=None):
-    if not charName:
+async def maple(ctx, char_name=None):
+    if not char_name:
         await ctx.send(f"{ctx.message.author.mention} 사용법: !메이플 <캐릭터 이름>")
         return
 
-    charInfo = get_character_info(charName)
-    if not charInfo:
+    char_info = get_character_info(char_name)
+    if not char_info:
         await ctx.send(
             embed=discord.Embed(description="존재하지 않는 캐릭터입니다.", color=0xFF9D00)
         )
         return
 
-    charInfoEmbed = discord.Embed(title=charInfo["이름"], color=0xFF9D00)
-    charInfoEmbed.set_image(
-        url=charInfo["캐릭터 이미지"].replace("https://", "http://")  # .replace("/180", "")
+    char_info_embed = discord.Embed(title=char_info["이름"], color=0xFF9D00)
+    char_info_embed.set_image(
+        url=char_info["캐릭터 이미지"].replace("https://", "http://")  # .replace("/180", "")
     )
     for key in maplerefs.display_keys:
-        if key in charInfo.keys():
-            charInfoEmbed.add_field(name=key, value=charInfo[key], inline=True)
-    await ctx.send(embed=charInfoEmbed)
+        if key in char_info.keys():
+            char_info_embed.add_field(name=key, value=char_info[key], inline=True)
+    await ctx.send(embed=char_info_embed)
 
 
 async def connect_helper(ctx):
@@ -86,31 +92,36 @@ async def disconnect(ctx):
 
 
 @bot.command(aliases=["재생"])
-async def play(ctx, user_input=None):
+async def play(ctx, *user_input):
     if not user_input:
         # Argument가 없고, pause 상태일 경우 음악 재생을 재개함
         if ctx.voice_client and ctx.voice_client.is_paused():
             await ctx.send(f"{ctx.message.author.mention} 음악을 다시 재생합니다.")
             ctx.voice_client.resume()
         else:
-            await ctx.send(f"{ctx.message.author.mention} 사용법: !재생 <YouTube URL>")
+            await ctx.send(
+                f"{ctx.message.author.mention} 사용법: !재생 <YouTube URL 또는 키워드>"
+            )
             return
 
     # 음성 채널에 연결
     await connect_helper(ctx)
 
     # 유튜브로부터 음악 다운로드
-    url = download_from_youtube(user_input)
+    URL = download_from_youtube(user_input)
+    if not URL:
+        await ctx.send(f"{ctx.message.author.mention} 올바르지 않은 YouTube URL 또는 키워드입니다.")
+        return
 
     # 음악 재생
     FFMPEG_OPTIONS = {
         "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
         "options": "-vn",
     }
-    await ctx.send(f"{ctx.message.author.mention} 음악 재생 중...")
     if ctx.voice_client.is_playing() or ctx.voice_client.is_paused():
         ctx.voice_client.stop()
-    ctx.voice_client.play(discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS))
+    await ctx.send(f"{ctx.message.author.mention} 음악 재생 준비 중...")
+    ctx.voice_client.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
 
 
 @bot.command(aliases=["일시정지"])
--- ydl.py
+++ ydl.py
@@ -5,9 +5,16 @@ from youtube_search import YoutubeSearch
 def download_from_youtube(user_input):
     with youtube_dl.YoutubeDL({"format": "bestaudio"}) as ydl:
         try:
-            info = ydl.extract_info(user_input, download=False)
-            url = info["formats"][0]["url"]
-            return url
+            info = ydl.extract_info(user_input[0], download=False)
+            URL = info["formats"][0]["url"]
+            return URL
         except:
-            # results = YoutubeSearch(user_input, max_result=1).to_dict()
-            return None
+            kwd_str = "".join(str(s) + " " for s in user_input)[:-1]
+            results = YoutubeSearch(kwd_str, max_results=1).to_dict()
+            url = "https://www.youtube.com" + results[0]["url_suffix"]
+            try:
+                info = ydl.extract_info(url, download=False)
+                URL = info["formats"][0]["url"]
+                return URL
+            except:
+                return None
